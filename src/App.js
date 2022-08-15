64f474e2f7dfe027fa91b8c53d5c949a850079f4
IddrisuAminu <iddrisuaminuict@gmail.com>
2022-08-15 17:06:28 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from './components/NavBar'
+import Todo from './components/Todo'
 
 function App() {
+
+  let title ="My activities to do for the week"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar title={title} />
+      <main>
+        <Todo title="learning react" datetime="15th August 2022" />
+        <Todo title="learning javascript" datetime="15th August 2022" />
+        <Todo title="learning figma" datetime="15th August 2022" />
+        <Todo title="learning aws" datetime="15th August 2022" />
+        <Todo title="learning css" datetime="15th August 2022" />
+        
+</main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
